74b8e9cee21278859a21b27e80a54aab62388e96
carlos.padron <carlos.padron@gigaclear.com>
2024-07-23 15:43:13 +0100

    adding legend components

---
+++ webgis_angular/src/app/map-legend-item/map-legend-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-legend-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './map-legend-item.component.html',
+  styleUrl: './map-legend-item.component.css'
+})
+export class MapLegendItemComponent {
+
+}
---
+++ webgis_angular/src/app/map-legend/map-legend.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-legend',
+  standalone: true,
+  imports: [],
+  templateUrl: './map-legend.component.html',
+  styleUrl: './map-legend.component.css'
+})
+export class MapLegendComponent {
+
+}
