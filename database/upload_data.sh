360f014f4ae7f4fbe7ff1a54acf641f08ce7c246
carlospadron <padron.ca@gmail.com>
2024-12-11 16:10:16 +0000

    fixing typo

--- database/upload_data.sh
+++ database/upload_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Upload GeoPackage to PostgreSQL
-ogr2ogr -f "PostgreSQL" PG:"host=$DB_HOST port=$DB_PORT dbname=$DB_NAME user=$DB_USER password=$DB_PASSWORD" "$GEOPACKAGE_PATH"
+ogr2ogr -f "PostgreSQL" PG:"host=$DB_HOST port=$DB_PORT dbname=$DB_NAME user=$DB_USER password=$DB_PASSWORD" -lco SCHEMA=$SCHEMA_NAME "$GEOPACKAGE_PATH"
 
 echo "GeoPackage uploaded to PostgreSQL database '$DB_NAME' successfully."
