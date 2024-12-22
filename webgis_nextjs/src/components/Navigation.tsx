70c46ee912c10b97ad3558f0a7851137db18ddb3
carlospadron <padron.ca@gmail.com>
2024-12-22 07:34:21 +0000

    simplifying it

--- webgis_nextjs/src/components/Navigation.tsx
+++ webgis_nextjs/src/components/Navigation.tsx
@@ -4,9 +4,6 @@ import {
   setItemActive
 } from "baseui/app-nav-bar";
 import {
-  ChevronDown,
-  Delete,
-  Overflow,
   Upload
 } from "baseui/icon";
 
@@ -14,44 +11,16 @@ export default () => {
   const [mainItems, setMainItems] = React.useState([
     {
       icon: Upload,
-      label: "Main A"
-    },
-    {
-      active: true,
-      icon: ChevronDown,
-      label: "Main B",
-      navExitIcon: Delete,
-      children: [
-        {
-          icon: Upload,
-          label: "Secondary A"
-        },
-        {
-          icon: Upload,
-          label: "Secondary B"
-        }
-      ]
+      label: "Layers"
     }
   ]);
   return (
     <AppNavBar
-      title="Title"
+      title="Webmap"
       mainItems={mainItems}
       onMainItemSelect={item => {
         setMainItems(prev => setItemActive(prev, item));
       }}
-      username="Umka Marshmallow"
-      usernameSubtitle="5 Stars"
-      userItems={[
-        {
-          icon: Overflow,
-          label: "User A"
-        },
-        {
-          icon: Overflow,
-          label: "User B"
-        }
-      ]}
       onUserItemSelect={item => console.log(item)}
     />
   );
