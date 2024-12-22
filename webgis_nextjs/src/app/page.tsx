73472255d11cc52e19792b1b8e0d658c35cc8ede
carlospadron <padron.ca@gmail.com>
2024-12-22 07:17:48 +0000

    top navigation

--- webgis_nextjs/src/app/page.tsx
+++ webgis_nextjs/src/app/page.tsx
@@ -1,28 +1,39 @@
 "use client";
 
-import Map from "../components/Map";
-import Navigation from "@/components/Navigation";
 import { Client as Styletron } from "styletron-engine-monolithic";
 import { Provider as StyletronProvider } from "styletron-react";
 import { LightTheme, BaseProvider, styled } from "baseui";
+import Navigation from "@/components/Navigation";
+import Map from "../components/Map";
 
 const engine = new Styletron();
 
-const Centered = styled("div", {
+const Container = styled("div", {
   display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  height: "100%",
+  flexDirection: "column",
+  height: "100vh",
+});
+
+const NavigationWrapper = styled("div", {
+  flex: "0 1 auto",
+});
+
+const MapWrapper = styled("div", {
+  flex: "1 1 auto",
 });
 
 export default function Home() {
   return (
     <StyletronProvider value={engine}>
       <BaseProvider theme={LightTheme}>
-        <Centered>
-          <Navigation />
-          <Map />
-        </Centered>
+        <Container>
+          <NavigationWrapper>
+            <Navigation />
+          </NavigationWrapper>
+          <MapWrapper>
+            <Map />
+          </MapWrapper>
+        </Container>
       </BaseProvider>
     </StyletronProvider>
   );
--- webgis_nextjs/src/components/Navigation.tsx
+++ webgis_nextjs/src/components/Navigation.tsx
@@ -1,49 +1,58 @@
-"use client";
-
 import * as React from "react";
 import {
-  BottomNavigation,
-  NavItem
-} from "baseui/bottom-navigation";
-import { Calendar, Search, Show } from "baseui/icon";
+  AppNavBar,
+  setItemActive
+} from "baseui/app-nav-bar";
+import {
+  ChevronDown,
+  Delete,
+  Overflow,
+  Upload
+} from "baseui/icon";
 
 export default () => {
-  const [activeKey, setActiveKey] = React.useState(0);
+  const [mainItems, setMainItems] = React.useState([
+    {
+      icon: Upload,
+      label: "Main A"
+    },
+    {
+      active: true,
+      icon: ChevronDown,
+      label: "Main B",
+      navExitIcon: Delete,
+      children: [
+        {
+          icon: Upload,
+          label: "Secondary A"
+        },
+        {
+          icon: Upload,
+          label: "Secondary B"
+        }
+      ]
+    }
+  ]);
   return (
-    <BottomNavigation
-      activeKey={activeKey}
-      onChange={({ activeKey }) => {
-        setActiveKey(activeKey);
+    <AppNavBar
+      title="Title"
+      mainItems={mainItems}
+      onMainItemSelect={item => {
+        setMainItems(prev => setItemActive(prev, item));
       }}
-    >
-      <NavItem title="First" icon={Calendar}>
-        <h1>First page</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur
-          adipiscing elit, sed do eiusmod tempor
-          incididunt ut labore et dolore magna aliqua
-          libero tempore, cum soluta nobis est eligendi
-          optio.
-        </p>
-      </NavItem>
-      <NavItem title="Second" icon={Search}>
-        <h1>Second page</h1>
-        <p>
-          At vero eos et accusamus et iusto odio
-          dignissimos ducimus qui blanditiis praesentium
-          voluptatum deleniti atque corrupti quos dolores
-          et quas molestias excepturi sint occaecati.
-        </p>
-      </NavItem>
-      <NavItem title="Third" icon={Show}>
-        <h1>Third page</h1>
-        <p>
-          Itaque earum rerum hic tenetur a sapiente
-          delectus, ut aut reiciendis voluptatibus maiores
-          alias consequatur aut perferendis doloribus
-          asperiores repellat.
-        </p>
-      </NavItem>
-    </BottomNavigation>
+      username="Umka Marshmallow"
+      usernameSubtitle="5 Stars"
+      userItems={[
+        {
+          icon: Overflow,
+          label: "User A"
+        },
+        {
+          icon: Overflow,
+          label: "User B"
+        }
+      ]}
+      onUserItemSelect={item => console.log(item)}
+    />
   );
 }
