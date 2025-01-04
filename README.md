f1f79338c9d5c15225b7fd25c9d1c00264a9eafc
carlospadron <padron.ca@gmail.com>
2025-01-04 10:49:16 +0000

    move env var load to readme

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # maplibre_tempalte
 A template for a webgis portal made with maplibre
 There is a folder for Angular and one for Vue
+
+
+# Load environment variables from .env file
+```
+export $(grep -v '^#' .env | xargs)
+```
--- database/upload_data.sh
+++ database/upload_data.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Load environment variables from .env file
-export $(grep -v '^#' .env | xargs)
-
 # Upload GeoPackage to PostgreSQL
 ogr2ogr -f "PostgreSQL" PG:"host=$DB_HOST port=$DB_PORT dbname=$DB_NAME user=$DB_USER password=$DB_PASSWORD" -lco SCHEMA=$SCHEMA_NAME "$GEOPACKAGE_PATH"
 
